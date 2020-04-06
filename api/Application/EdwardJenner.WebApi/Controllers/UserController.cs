5f163ea37e6fbd0c5c6261fc107f6166859245fa
Lennon Alves Dias <lennonalvesdias@gmail.com>
2020-04-06 00:57:53 -0300

    remove not used

--- api/Application/EdwardJenner.WebApi/Controllers/UserController.cs
+++ api/Application/EdwardJenner.WebApi/Controllers/UserController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using EdwardJenner.Domain;
 using EdwardJenner.Domain.Interfaces.Repositories;
 using EdwardJenner.Domain.Interfaces.Services;
 using EdwardJenner.Models.Models;
